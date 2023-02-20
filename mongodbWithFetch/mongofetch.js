502a9dbf61b8d1f010777c150abde7f36dcc1412
nickolasram <nickolasram@gmail.com>
2023-02-19 21:02:20 -0800

    mongofetch

---
+++ mongodbWithFetch/mongofetch.js
@@ -0,0 +1,30 @@
+const returnApartments = async() =>{
+    const api = await fetch ("https://data.mongodb-api.com/app/data-hejgp/endpoint/data/v1/action/find",
+        { 
+            method: "POST",
+            credentials: "same-origin",
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Request-Headers": "*",
+                "api-key": "LbDURdvoaYeDAFSfYAqUraCyVFVN89I8SNMxb5BEhrBUIeGJZfI2bqlJz3jAxchw"
+            },
+            body: {
+                "data-raw": {
+                    "collection":"listingsAndReviews",
+                    "database":"sample_airbnb",
+                    "dataSource":"Cluster0",
+                    "filter": {"property_type": "Apartment"},
+                    "projection": {
+                        "name": 1,
+                        "listing_url": 1,
+                        "summary": 1,
+                        "property_type": 1,
+                         "cancellation_policy": 1
+                    }
+                }
+            }
+        })
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
+returnApartments();
